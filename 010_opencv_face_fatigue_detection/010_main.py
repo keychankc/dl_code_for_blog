2817ca66f83fbffae2b52e639d884a64d30b6bd8
KeyChan <keychankc@126.com>
2025-04-14 15:48:24 +0800

    add face fatigue

---
+++ 010_opencv_face_fatigue_detection/010_main.py
@@ -0,0 +1,6 @@
+import detect_face_parts
+import detect_blinks
+
+if __name__ == '__main__':
+    # detect_face_parts.detect_face_part("images/liudehua.jpg")
+    detect_blinks.detect_blinks("test.mp4")
